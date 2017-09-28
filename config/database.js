bc59ce0673c71d824fc986bcbd58126e17ef85ff
OmarAguinaga <omar.aguinaga94@gmail.com>
2017-09-28 09:34:32 -0500

    Add local authentication with passport

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+// config/database.js
 module.exports = {
   url: 'mongodb://admin:password1234@ds153494.mlab.com:53494/hoopit'
 };
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 // set up ======================================================================
 // get all the tools we need
 var express = require('express');
@@ -17,24 +19,30 @@ var configDB = require('./config/database.js');
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'hakjshljkhsadljkhsadljs' })); // session secret
+app.use(
+  session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport);
-// load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
